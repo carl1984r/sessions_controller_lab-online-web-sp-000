2f6d1657d02a5dd1aef87fbc99ac29384947a799
Bry Cox <carl.cc@gmail.com>
2019-08-21 23:08:26 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,14 @@
 class SessionsController < ApplicationController
 
   def create
+    
    if params[:name].nil? || params[:name].empty?
      redirect_to :login
    else
      session[:name] = params[:name]
      redirect_to '/'
    end
+   
  end
 
 end
